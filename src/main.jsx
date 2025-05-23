588cb1e72101903faf994f1b88b34e1a19b24c4c
saimzaidi110 <saimzaidi110786@gmail.com>
2025-05-23 14:03:35 +0500

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Counter from './counter.jsx'
+import Counter from './Counter.jsx'
 import UseEffect from './UseEffect.jsx'
 import CategoryPage from './CategoryPage.jsx'
 
